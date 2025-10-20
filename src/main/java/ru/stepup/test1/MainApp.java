824b81e8d132552086ff6113b8880026768c67c6
ALamzova <ALamzova@inno.tech>
2025-10-20 17:04:34 +0300

    доработка

--- src/main/java/ru/stepup/test1/MainApp.java
+++ src/main/java/ru/stepup/test1/MainApp.java
@@ -7,8 +7,9 @@ public class MainApp {
     }
 
     public static void main(String[] args) {
-        System.out.println(1);
+        System.out.println("Hello World");
         Cat cat = new Cat();
+        System.out.println("12345");
 
     }
 }
