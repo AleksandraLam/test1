57bcf0fb22b005000e7e699de4051e52a1ef34f8
ALamzova <ALamzova@inno.tech>
2025-10-21 09:48:59 +0300

    доработка2 box

--- src/main/java/com/stepup/proj/App.java
+++ src/main/java/com/stepup/proj/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("12345");
+        System.out.println("00000");
     }
 }
