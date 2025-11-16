2b58f9c3f146631c4f83fe3b6a55b3430dbc506c
ALamzova <ALamzova@inno.tech>
2025-11-16 17:35:49 +0300

    доработка5 "---" and 111

--- src/main/java/com/stepup/proj/App.java
+++ src/main/java/com/stepup/proj/App.java
@@ -2,16 +2,17 @@ package com.stepup.proj;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("Hello World");
         System.out.println("12345");
-        System.out.println("00000");
+        System.out.println("00001");
     }
 
     public static void doSomething() {
 
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
+        System.out.println(1);
 
     }
 }
